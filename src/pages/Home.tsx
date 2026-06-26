6f8bd695f258c19fdc059da51a36417b501314e5
TianmingLe <TianmingLe@users.noreply.github.com>
2026-06-26 14:10:15 +0000

    feat: 查看项目
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,27 +6,18 @@ import Timeline from "@/components/Timeline";
 import Awards from "@/components/Awards";
 import Strengths from "@/components/Strengths";
 import Contact from "@/components/Contact";
-import ClickSpark from "@/components/ClickSpark";
 
 export default function Home() {
   return (
     <main className="relative min-h-screen w-full bg-ink-950 pb-20">
-      <ClickSpark
-        sparkColor="#7DD3FC"
-        sparkSize={15}
-        sparkRadius={25}
-        sparkCount={8}
-        duration={500}
-      >
-        <Navbar />
-        <Hero />
-        <About />
-        <Awards />
-        <AbilityMap />
-        <Timeline />
-        <Strengths />
-        <Contact />
-      </ClickSpark>
+      <Navbar />
+      <Hero />
+      <About />
+      <Awards />
+      <AbilityMap />
+      <Timeline />
+      <Strengths />
+      <Contact />
     </main>
   );
 }
