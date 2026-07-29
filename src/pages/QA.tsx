2c0b19cd781c0777aadb952a5ec40d8ff2acb18a
TianmingLe <TianmingLe@users.noreply.github.com>
2026-07-29 14:03:28 +0000

    Revert commits a76e89d..a76e89d
    This reverts 1 commit(s) from a76e89d to a76e89d.
    
    Reverted commits:
    a76e89d feat: 拉取并查看最新项目
    
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- src/pages/QA.tsx
+++ src/pages/QA.tsx
@@ -36,7 +36,7 @@ export default function QA() {
                 gravity={[0, -40, 0]}
                 frontImage="/正面照2.webp"
                 imageFit="contain"
-                imageScale={1.1495}
+                imageScale={1.045}
                 frontText="HU.YW"
                 lanyardWidth={0.6}
               />
